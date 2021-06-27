5b904c9330f7aeffa9f8ccf252ef6d856fdc9545
debsoumya22 <69747102+debsoumya22@users.noreply.github.com>
2021-06-28 00:17:03 +0530

    Add files via upload

---
+++ Stage 2/src/bot/SimpleBot.java
@@ -0,0 +1,16 @@
+package bot;
+
+import java.util.Scanner;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2020.");
+        System.out.println("Please, remind me your name.");
+
+        Scanner sc  = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("What a great name you have, " + name + "!");    }
+}
